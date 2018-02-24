8f1fb9cebccd1e71244a14fec99cf2cf928c4f8d
Woramet Muangsiri <krw127@hotmail.com>
2018-02-24 11:19:38 +0900

    Initial Commit

---
+++ ThaiTechEventsCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThaiTechEventsCalendar
+//
+//  Created by Woramet Muangsiri on 2/24/18.
+//  Copyright © 2018 WM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
