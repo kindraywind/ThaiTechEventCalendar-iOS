79613dc06030d033194ac95f5a905fad4bc524ef
Woramet Muangsiri <krw127@hotmail.com>
2018-03-01 01:18:49 +0900

    Add tabbar

---
+++ ThaiTechEventsCalendar/ViewControllers/SettingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingViewController.swift
+//  ThaiTechEventsCalendar
+//
+//  Created by Woramet Muangsiri on 3/1/18.
+//  Copyright © 2018 WM. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
