f170b3bf5afa732609201def62fea2904e1a34c9
Woramet Muangsiri <krw127@hotmail.com>
2018-10-04 00:52:34 +0900

    remove -quiet from runtest script

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 xcrun simctl uninstall booted com.octto.ThaiTechEventsCalendar;
-xcodebuild test -quiet -workspace ThaiTechEventsCalendar.xcworkspace -scheme ThaiTechEventsCalendarTests CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 7' | xcpretty -c;
+xcodebuild test -workspace ThaiTechEventsCalendar.xcworkspace -scheme ThaiTechEventsCalendarTests CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 7' | xcpretty -c;
