0d00789b8065ecec4e03fc2133643395149f8f19
Woramet Muangsiri <krw127@hotmail.com>
2018-10-04 00:35:39 +0900

    Add $(FRAMEWORK_SEARCH_PATHS)

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 xcrun simctl uninstall booted com.octto.ThaiTechEventsCalendar;
-xcodebuild test -quiet -workspace ThaiTechEventsCalendar.xcworkspace -scheme ThaiTechEventsCalendarTests CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 7';
+xcodebuild test -quiet -workspace ThaiTechEventsCalendar.xcworkspace -scheme ThaiTechEventsCalendarTests CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 7' | xcpretty -c;
